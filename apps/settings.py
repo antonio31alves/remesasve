e61c7a3299c99bd450f6d5b0f54de45bf3d5248d
Antonio Alves Oropeza <antonio31alves@gmail.com>
2021-09-17 16:58:24 +0100

    Update settings.py

--- apps/settings.py
+++ apps/settings.py
@@ -199,5 +199,5 @@ EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'info.remesasve@gmail.com'
-EMAIL_HOST_PASSWORD = 'Lenovo90'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
